a7baf05a2c7c80e3b5c1f43857313da4aa8f4efc
Alexandre CMC Souza <ale@alemacedo.com>
2017-09-15 20:58:59 -0300

    Ainda não funcionou o firebase... :´(

--- app/src/main/java/com/alemacedo/patago/MainActivity.java
+++ app/src/main/java/com/alemacedo/patago/MainActivity.java
@@ -13,6 +13,8 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -25,10 +27,11 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Timer;
 
 import com.alemacedo.patago.fragments.TabFragment;
-import com.alemacedo.patago.models.PatagoRotinas;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -39,14 +42,16 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final Object TAG = "ALEMACEDO";
+
     Toolbar toolbar;
     DrawerLayout mDrawerLayout;
     NavigationView mNavigationView;
     FragmentManager mFragmentManager;
     FragmentTransaction mFragmentTransaction;
-    // private InterstitialAd interstitial;
-    public Timer AdTimer;
+    // Não sei onde isso é usado, daí comentei //private InterstitialAd interstitial;
+    // Não sei onde isso é usado, daí comentei //public Timer AdTimer;
+
+    // Retrofit: RecyclerView, ArrayList, BASE_URL, DataAdapter
 
     SharedPreferences preferences;
 
@@ -59,32 +64,8 @@ public class MainActivity extends AppCompatActivity {
 
         preferences = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
 
-        //recupera dados ===========================================================================================
 
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(PatagoRoutineService.BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
 
-        PatagoRoutineService service = retrofit.create(PatagoRoutineService.class);
-        Call<PatagoRotinas> requestRotinas = service.ListRoutines();
-
-        requestRotinas.enqueue(new Callback<PatagoRotinas>() {
-            @Override
-            public void onResponse(Call<PatagoRotinas> call, Response<PatagoRotinas> response) {
-                if(!response.isSuccessful()) {
-                    Log.i("TAG","deu erro:" + response.code());
-                } else {
-                    // Deu certo!
-                    PatagoRotinas rotinas = response.body();
-                }
-            }
-
-            @Override
-            public void onFailure(Call<PatagoRotinas> call, Throwable t) {
-                Log.e("TAG","Erro: " + t.getMessage());
-            }
-        });
 
 
         // LIDA COM NAVDRAWER ==================================================================================
@@ -96,7 +77,6 @@ public class MainActivity extends AppCompatActivity {
 
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mNavigationView = (NavigationView) findViewById(R.id.main_drawer);
-
 
         // CARREGA OS FRAGMENTS DE ACORDO COM A OPÇÃO DO USUÁRIO
 
@@ -149,11 +129,11 @@ public class MainActivity extends AppCompatActivity {
                    fragmentTransaction.replace(R.id.frame_container, new About()).commit();
                 }
 
-     /*             if (menuItem.getItemId() == R.id.drawer_share) {
-                    Toast.makeText(MainActivity.this,
-                            "Replace with your own function", Toast.LENGTH_LONG).show();
+                 if (menuItem.getItemId() == R.id.nav_myinfo) {
+                     FragmentTransaction fragmentTransaction = mFragmentManager.beginTransaction();
+                     fragmentTransaction.replace(R.id.frame_container, new MyData()).commit();
                 }
-                if (menuItem.getItemId() == R.id.drawer_youtube) {
+                /* if (menuItem.getItemId() == R.id.drawer_youtube) {
                     Toast.makeText(MainActivity.this,
                             "Replace with your own function", Toast.LENGTH_LONG).show();
                 }
@@ -176,10 +156,11 @@ public class MainActivity extends AppCompatActivity {
         mDrawerToggle.syncState();
 
 
+
     }
 
 
-    // Lida com o botão Chamar do fragment Amigo de Confiança ================================
+    // Lida com o botão FAZER CHAMADA do fragment Amigo de Confiança ================================
 
     public void btCallClick (View v) {
         Uri number = Uri.parse("tel:11985527281");
@@ -199,122 +180,3 @@ public class MainActivity extends AppCompatActivity {
     }
 }
 
-
-
-
-
-/*
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
-
-
-
-
-
-
-
-    private ViewPager viewPager;
-    private DrawerLayout drawer;
-    private TabLayout tabLayout;
-    private String[] pageTitle = {"Buscar Rotinas", "Minhas Rotinas", "Meu Dia"};
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        // viewPager = (ViewPager)findViewById(R.id.view_pager);
-
-
-        // tablayout titles
-        tabLayout = (TabLayout) findViewById(R.id.tablayout);
-        for (int i = 0; i < 3; i++) {
-            tabLayout.addTab(tabLayout.newTab().setText(pageTitle[i]));
-        }
-        tabLayout.setTabGravity(TabLayout.GRAVITY_CENTER);
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-
-    }
-
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.nav_about) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-        Fragment fragment = null;
-
-        if (id == R.id.nav_panic) {
-            fragment = new Panic();
-        } else if (id == R.id.nav_trusted) {
-            fragment = new TrustedFriendFragment();
-        } else if (id == R.id.nav_myinfo) {
-            fragment = new BlankFragment(); // TODO
-        } else if (id == R.id.nav_rotinas) {
-            fragment = new Rotinas();
-        } else if (id == R.id.nav_about) {
-            fragment = new BlankFragment(); // TODO
-        }
-        else if (id == R.id.nav_send) {
-            fragment = new BlankFragment(); // TODO
-        }
-        else if (id == R.id.nav_share) {
-            fragment = new BlankFragment(); // TODO
-        }
-        else if (id == R.id.nav_settings) {
-            fragment = new BlankFragment(); // TODO
-        };
-        if (fragment != null) {
-            FragmentManager fragmentManager = getSupportFragmentManager();
-            fragmentManager.beginTransaction()
-                    .replace(R.id.frame, fragment).commit();
-
-        }
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
-    }
-}
-*/
--- app/src/main/java/com/alemacedo/patago/Rotinas.java
+++ app/src/main/java/com/alemacedo/patago/Rotinas.java
@@ -1,26 +1,111 @@
 package com.alemacedo.patago;
 
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 
 public class Rotinas extends Fragment {
 
 
-    public Rotinas() {
-        // Required empty public constructor
-    }
+    private static final String TAG = "ALEMACEDO";
+    public static final String BASE_URL = "http://www.mocky.io/v2/";
 
+    private RecyclerView mRecyclerView;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private ArrayList<AndroidVersion> data;
+    private DataAdapter adapter;
+
+    // Required empty public constructor
+    public Rotinas() {
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_rotinas, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_rotinas, container, false);
+        rootView.setTag(TAG);
+
+        // inicializa a RecyclerView e o layoutmanager
+        mRecyclerView = (RecyclerView) rootView.findViewById(R.id.card_recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+
+        mLayoutManager = new LinearLayoutManager(getContext());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+       // mLayoutManager = new LinearLayoutManager(getActivity());
+
+        loadJSON();
+
+        return rootView;
+
+    }
+
+    private void loadJSON(){
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(RequestInterface.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        RequestInterface request = retrofit.create(RequestInterface.class);
+        Call<JSONResponse> call = request.getJSON();
+
+        // Cria o progress
+        final ProgressDialog progressDoalog;
+        progressDoalog = new ProgressDialog(getContext());
+        progressDoalog.setTitle("Carregando rotinas...");
+        progressDoalog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        progressDoalog.show();
+        // ^^ mostra o progress
+
+
+        call.enqueue(new Callback<JSONResponse>() {
+            @Override
+            public void onResponse(Call<JSONResponse> call, Response<JSONResponse> response) {
+            if (response.isSuccessful()) {
+                JSONResponse jsonResponse = response.body();
+                data = new ArrayList<AndroidVersion>();
+                for (AndroidVersion av : jsonResponse.android) {
+                    data.add(av);
+                }
+                adapter = new DataAdapter(data);
+                mRecyclerView.setAdapter(adapter);
+            } else {
+                Log.e(TAG, "Erro no JSON");
+            }
+
+            progressDoalog.dismiss();
+
+            }
+
+            @Override
+            public void onFailure(Call<JSONResponse> call, Throwable t) {
+                Log.d("Error",t.getMessage());
+                progressDoalog.dismiss();
+            }
+        });
     }
 
 }
+
+//
